8479f5e8860d908f3e7c65225fdd9e9c9d8cbbcd
mohif9232 <mohif.waghu@gmail.com>
2022-12-02 17:30:39 +0530

    first commit

---
+++ controller/product.js
@@ -0,0 +1,161 @@
+let uploads= require("../helper/file")
+let {addproduct,updateproduct,activeproduct,inactiveproduct,deleteproduct,undeleteproduct,findproduct}=require("../model/product")
+const path=require("path");
+const { array } = require("joi");
+const app = require("../init/cors");
+let {Product}= require("../schema/product")
+let excel= require("../helper/excel");
+
+
+// async function add_product(request,response){
+//     let addpic= await uploads(request,response,"avatar",{fileSize:3*1000*1000}).catch((err)=>{
+//         return {error:err}
+//     });
+//     if(!addpic || addpic.error){
+//         return response.status(401).send({error:addpic.error})
+//     }
+//     // return response.send(addpic)
+//     console.log(addpic)
+//     let add=await addproduct(request.body,addpic.path).catch((err)=>{
+//         return ({error:err})
+//     });
+
+//     if(!add || add.error){
+//         return response.status(401).send({error:add.error})
+//     }
+//     return response.send({data:add.data})
+// }
+async function add_product(request,response){
+
+    let addpic= await uploads(request,response,[{name:"product",maxCount:4}],{destination:'./product-pic/',fileSize:3*1000*1000}).catch((err)=>{
+        return {error:err}
+    });
+    
+    if(!addpic || addpic.error){
+        return response.status(401).send({error:addpic.error})
+    }
+    console.log(addpic)
+    let data=[]
+    for(let i of addpic.product){
+        data.push(i.path)
+    }
+
+    let path = data.join("   AND   ");
+
+    let add=await addproduct(request.body,path,request.userData).catch((err)=>{
+        return ({error:err})
+    });
+
+    if(!add || add.error){
+        return response.status(401).send({error:add.error})
+    }
+    return response.send({data:add.data})
+}
+
+async function update_product(request,response){
+    let addpic= await uploads(request,response,[{name:"product",maxCount:4}],{destination:'./product-pic/',fileSize:3*1000*1000}).catch((err)=>{
+        return {error:err}
+    });
+    
+    if(!addpic || addpic.error){
+        return response.status(401).send({error:addpic.error})
+    }
+    
+    let data=[]
+    for(let i of addpic.product){
+        data.push(i.path)
+    }
+
+    let path = data.join("   AND   ");
+
+    let add=await updateproduct(request.body,path,request.userData).catch((err)=>{
+        return {error:err}
+    });
+    if(!add || add.error){
+        return response.status(401).send({error:add.error})
+    }
+    return response.send({data:add.data})
+}
+
+
+async function active_product(request,response){
+    let add=await activeproduct(request.body,request.userData).catch((err)=>{
+        return {error:err}
+    });
+    if(!add || add.error){
+        return response.status(401).send({error:add.error})
+    }
+    return response.send({data:add.data})
+}
+
+async function inactive_product(request,response){
+    let add=await inactiveproduct(request.body,request.userData).catch((err)=>{
+        return {error:err}
+    });
+    if(!add || add.error){
+        return response.status(401).send({error:add.error})
+    }
+    return response.send({data:add.data})
+}
+
+async function delete_product(request,response){
+    let add=await deleteproduct(request.body,request.userData).catch((err)=>{
+        return {error:err}
+    });
+    if(!add || add.error){
+        return response.status(401).send({error:add.error})
+    }
+    return response.send({data:add.data})
+}
+
+async function undelete_product(request,response){
+    let add=await undeleteproduct(request.body,request.userData).catch((err)=>{
+        return {error:err}
+    });
+    if(!add || add.error){
+        return response.status(401).send({error:add.error})
+    }
+    return response.send({data:add.data})
+}
+
+async function find_product(request,response){
+    let add=await findproduct(request.body).catch((err)=>{
+        return {error:err}
+    });
+    if(!add || add.error){
+        return response.status(401).send({error:add.error})
+    }
+    return response.send({data:add.data})
+}
+
+async function exportProduct(request,response){
+    let productData= await Product.findAll({raw:true}).catch((err)=>{
+        return { error: err}
+    });
+    if(!productData || productData.error){
+        return response.status(500).send({error:"Internal Server Error"})
+    }
+    let columns=[
+        { header: 'id', key: 'id', width: 10 },
+        { header: 'name', key: 'name', width: 10 },
+        { header: 'quantity', key: 'quantity', width: 10 },
+        { header: 'price', key: 'price', width: 10 },
+        { header: 'discount', key: 'discounted_price', width: 10 },
+        { header: 'img_path', key: 'img_path', width: 10 },
+        { header: 'stock_alert', key: 'stock_alert', width: 10 },
+    ]
+
+    let filename= "product";
+    await excel(request,response,filename,columns,productData)
+}
+
+module.exports={
+    add_product,
+    update_product,
+    active_product,
+    inactive_product,
+    delete_product,
+    undelete_product,
+    find_product,
+    exportProduct
+}
---
+++ route/user.js
@@ -0,0 +1,42 @@
+let {register,login, about_me,my_profile_pic,update_profile_pic,forgetpassword,resetpassword,changepassword,updatemyprofile,deactivateme}=require("../controller/user")
+let {categoryView}=require("../controller/category")
+let {find_product}=require("../controller/product")
+let {errorhandler}=require("../middleware/errorhandling")
+let { addCart,updateCart ,viewCart, removeCart}= require("../controller/cart")
+let {auth}=require("../middleware/authpermission")
+let cors= require("../init/cors")
+let express=require("express");
+require("express-async-errors")
+let app= express();
+
+app.use(cors);
+app.post("/login", login) //for login of admin
+app.post("/register", register) //for login of admin
+
+app.get("/about_me",auth("common"),about_me);
+app.post("/my_profile_pic",auth("common"),my_profile_pic);
+app.put("/update_my_profile_pic",auth("common"),update_profile_pic);
+
+app.get("/view_product",find_product)
+app.get("/view_category",categoryView)
+
+
+app.post("/add_to_cart",auth("common"),addCart);
+app.get("/view_cart",auth("common"),viewCart);
+app.put("/update_cart",auth("common"),updateCart);
+app.delete("/remove_from_cart",auth("common"),removeCart);
+
+
+
+app.post("/change_password",auth("common"),changepassword);
+app.post("/update_profile",auth("common"),updatemyprofile)
+
+app.post("/forget_password",forgetpassword);
+app.put("/reset_password",resetpassword);
+
+app.put("/deactivate_account",auth("common"),deactivateme);
+
+
+
+app.use(errorhandler)
+module.exports=app
